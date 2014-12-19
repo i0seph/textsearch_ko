b96b8799df91a3ac648e4ee365a8a99718eecf6d
Ioseph Kim <iosephkim@gmail.com>
2014-12-19 14:41:05 +0900

    change default schema to public

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export PATH=/data/pgsql/9.3/bin:/data/mecab-ko/bin:$PATH
+export PATH=/data/pgsql/9.4/bin:/data/mecab-ko/bin:$PATH
 C_INCLUDE_PATH=/data/mecab-ko/include LIBRARY_PATH=/data/mecab-ko/lib make USE_PGXS=1 install
